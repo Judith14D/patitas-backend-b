bf510ef7c67b8840fc8bcbd5a77b99c248536463
Judith14D <Judith.ramos.supanta@gmail.com>
2024-10-13 18:55:17 -0500

    Ultima actualización

---
+++ src/main/java/pe/edu/cibertec/patitas_backend_b/service/AutenticacionService.java
@@ -0,0 +1,11 @@
+package pe.edu.cibertec.patitas_backend_b.service;
+import pe.edu.cibertec.patitas_backend_b.dto.LoginRequestDTO;
+import pe.edu.cibertec.patitas_backend_b.dto.LogoutRequestDTO;
+import java.util.Date;
+import java.io.IOException;
+
+public interface AutenticacionService {
+
+    String[] validarUsuario(LoginRequestDTO loginRequestDTO) throws IOException;
+    Date cerrarSesionUsuario(LogoutRequestDTO logoutRequestDTO) throws IOException;
+}
